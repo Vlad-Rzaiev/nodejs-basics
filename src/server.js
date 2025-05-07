d94fcadad54b8d2502d5e7ae5f1d556db8a7706d
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2025-05-07 21:41:33 +0200

    add students model

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllStudents, getStudentById } from './services/students.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -27,6 +28,30 @@ export const startServer = () => {
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello World',
+    });
+  });
+
+  app.get('/students', async (req, res) => {
+    const students = await getAllStudents();
+
+    res.status(200).json({
+      data: students,
+    });
+  });
+
+  app.get('/students/:studentId', async (req, res) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
+
+    if (!student) {
+      res.status(404).json({
+        message: 'Student not found.',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      data: student,
     });
   });
 
