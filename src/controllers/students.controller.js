65a8c0ada0f292cbcb820ab22599e4ca25e435d2
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2025-05-21 21:46:44 +0200

    add filters

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -7,13 +7,20 @@ import {
   upsertStudent,
 } from '../services/students.service.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getStudentsController = async (req, res, next) => {
   const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
 
   const students = await getAllStudents({
     page,
     perPage,
+    sortBy,
+    sortOrder,
+    filter,
   });
 
   res.status(200).json({
--- src/services/students.service.js
+++ src/services/students.service.js
@@ -1,16 +1,54 @@
+import { SORT_ORDER } from '../constants/index.js';
 import { StudentCollection } from '../db/models/student.model.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllStudents = async ({ page, perPage }) => {
+export const getAllStudents = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
   const studentsQuery = StudentCollection.find();
-  const studentsCount = await StudentCollection.find()
-    .merge(studentsQuery)
-    .countDocuments();
 
-  const students = await studentsQuery.skip(skip).limit(limit).exec();
+  if (filter.gender) {
+    studentsQuery.where('gender').equals(filter.gender);
+  }
+  if (filter.maxAge) {
+    studentsQuery.where('age').lte(filter.maxAge);
+  }
+  if (filter.minAge) {
+    studentsQuery.where('age').gte(filter.minAge);
+  }
+  if (filter.maxAvgMark) {
+    studentsQuery.where('avgMark').lte(filter.maxAvgMark);
+  }
+  if (filter.minAvgMark) {
+    studentsQuery.where('avgMark').gte(filter.minAvgMark);
+  }
+
+  // const studentsCount = await StudentCollection.find()
+  //   .merge(studentsQuery)
+  //   .countDocuments();
+
+  // const students = await studentsQuery
+  //   .skip(skip)
+  //   .limit(limit)
+  //   .sort({ [sortBy]: sortOrder })
+  //   .exec();
+
+  const [studentsCount, students] = await Promise.all([
+    StudentCollection.find().merge(studentsQuery).countDocuments(),
+    studentsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
+
   const paginationData = calculatePaginationData(studentsCount, perPage, page);
 
   return {
